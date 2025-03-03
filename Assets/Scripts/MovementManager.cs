bcfee8a9fa8dc43cae6874ba02505298b0c4a058
KodeByNight <techstarkmk2@gmail.com>
2025-03-03 23:41:19 +0000

    Robot arm stufff

---
+++ Assets/Scripts/MovementManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementManager : MonoBehaviour
+{
+    //joint 1 object
+    //joint 2 object
+    //joint 3 object
+    //joint 4 object
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
