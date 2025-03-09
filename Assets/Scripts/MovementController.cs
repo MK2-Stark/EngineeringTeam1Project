bf1aed8016a94d104ec7739ed57bae2724d133b1
KodeByNight <techstarkmk2@gmail.com>
2025-03-09 01:29:59 +0000

    Y and Z angular rotation of each joint and limits within VR

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -4,10 +4,46 @@ using UnityEngine;
 
 public class MovementController : MonoBehaviour
 {
+
+    //joints
+    private Vector3 selectedJoint;
+
+    //Defined angular limitations of each joint
+    //private Vector3 minBaseLimit;
+    //private Vector3 maxBaseLimit;
+
+    //private Vector3 minShoulderLimit;
+    //private Vector3 maxShoulderLimit;
+
+    //private Vector3 minElbowLimit;
+    //private Vector3 maxElbowLimit;
+
+    //private Vector3 minWristLimit;
+    //private Vector3 maxWristLimit;
+
+    //Min Max values for rotation limits of joints
+    private bool isMinBase = false;
+    private bool isMaxBase = false;
+
+    private bool isMinShoulder = false;
+    private bool isMaxShoulder = false;
+
+    private bool isMinElbow = false;
+    private bool isMaxElbow = false;
+
+    private bool isMinWristVert = false;
+    private bool isMaxWristVert = false;
+
+
+    private void Awake()
+    {
+        
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       // GetBeginningAngularStateOfEachJoint();
     }
 
     // Update is called once per frame
@@ -15,4 +51,67 @@ public class MovementController : MonoBehaviour
     {
         
     }
+
+    //private void GetBeginningAngularStateOfEachJoint()
+    //{
+
+    //}
+
+    public void positiveYRotation(GameObject jointToMove)
+    {
+        //Joint angular local pivot rotation on +Y axis.
+        
+        selectedJoint = jointToMove.transform.localEulerAngles;
+        selectedJoint.y += 1;
+        if (selectedJoint.y <= 180)
+            jointToMove.transform.localEulerAngles = selectedJoint;
+    }
+
+
+    public void negativeYRotation(GameObject jointToMove)
+    {
+        //Joint angular local pivot rotation on -Y axis.
+        selectedJoint = jointToMove.transform.localEulerAngles;
+        selectedJoint.y -= 1;
+        if (selectedJoint.y >= 0)
+            jointToMove.transform.localEulerAngles = selectedJoint;
+    }
+
+    public void positiveZRotation(GameObject jointToMove)
+    {
+        //Joint angular local pivot rotation on +Z axis.
+        selectedJoint = jointToMove.transform.localEulerAngles;
+        selectedJoint.z += 1;
+        if (selectedJoint.z <= 75)
+            jointToMove.transform.localEulerAngles = selectedJoint;
+    }
+
+    public void negativeZRotation(GameObject jointToMove)
+    {
+        //Joint angular local pivot rotation on -Z axis.
+        selectedJoint = jointToMove.transform.localEulerAngles;
+        selectedJoint.z -= 1;
+        if (selectedJoint.z >= -75)
+            jointToMove.transform.localEulerAngles = selectedJoint;
+    }
+
+    //private bool minMaxBaseJointLimits()
+    //{
+    //    return false;
+    //}
+
+    //private bool minMaxShoulderJointLimits()
+    //{
+    //    return false;
+    //}
+
+    //private bool minMaxElbowJointLimits()
+    //{
+    //    return false;
+    //}
+
+    //private bool minMaxWristVertJointLimits()
+    //{
+    //    return false;
+    //}
 }
--- Assets/Scripts/MovementManager.cs
+++ Assets/Scripts/MovementManager.cs
@@ -1,25 +1,203 @@
+using Meta.XR.ImmersiveDebugger.UserInterface.Generic;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MovementManager : MonoBehaviour
 {
-    //joint 1 object
-    //joint 2 object
-    //joint 3 object
-    //joint 4 object
+    [SerializeField] private MovementController controller;
 
+    //joint selection bools
+    bool isBaseJoint = true;//base joint will be default selected joint
+    bool isShoulderJoint = false;
+    bool isElbowJoint = false;
+    bool isWristVertJoint = false;
 
+    //Buttons GameObjects
+    [SerializeField] private GameObject baseButton;
+    [SerializeField] private GameObject shoulderButton;
+    [SerializeField] private GameObject elbowButton;
+    [SerializeField] private GameObject wristVertButton;
 
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject minYButton;
+    [SerializeField] private GameObject posYButton;
+
+    [SerializeField] private GameObject minZButton;
+    [SerializeField] private GameObject posZButton;
+
+    [SerializeField] private GameObject sendButton;
+
+    //GameObjects
+    [SerializeField] private GameObject baseJoint;
+    private MeshRenderer baseRend;
+
+    [SerializeField] private GameObject shoulderJoint;
+    private MeshRenderer ShoulderRend;
+
+    [SerializeField] private GameObject elbowJoint;
+    private MeshRenderer elbowRend;
+
+    [SerializeField] private GameObject wristVertJoint;
+    private MeshRenderer wristVertRend;
+
+    //Materials
+    [SerializeField] private Material defaultMaterial;
+    [SerializeField] private Material selectedMaterial;
+
+    private void Awake()
     {
-        
+        //base joint is selected by default on Awake
+        baseJointSelected();
+
+        baseRend = baseJoint.GetComponent<MeshRenderer>();
+        ShoulderRend = shoulderJoint.GetComponent<MeshRenderer>();
+        elbowRend = elbowJoint.GetComponent<MeshRenderer>();
+        wristVertRend = wristVertJoint.GetComponent<MeshRenderer>();
+
     }
 
-    // Update is called once per frame
-    void Update()
+   
+
+    //Base Joint does local pivot rotate on Y axis.
+    public void minusYPressed()
     {
-        
+        if (isBaseJoint == true)
+        {
+            controller.negativeYRotation(baseJoint);
+        }
+    }
+
+    //Base Joint does local pivot rotate on Y axis only.
+    public void positveYPressed()
+    {
+        if (isBaseJoint == true)
+        {
+            controller.positiveYRotation(baseJoint);
+        }
+    }
+
+    //shoulder, elbow, and wristvert do local pivot rotate on Z axis only
+    public void minusZPressed()
+    {
+
+        if (isShoulderJoint == true)
+        {
+            controller.negativeZRotation(shoulderJoint);
+        }
+        else if (isElbowJoint == true)
+        {
+            controller.negativeZRotation(elbowJoint);
+        }
+        else if (isWristVertJoint == true)
+        {
+            controller.negativeZRotation(wristVertJoint);
+        }
+    }
+
+    public void positveZPressed()
+    {
+        if (isShoulderJoint == true)
+        {
+            controller.positiveZRotation(shoulderJoint);
+        }
+        else if (isElbowJoint == true)
+        {
+            controller.positiveZRotation(elbowJoint);
+        }
+        else if (isWristVertJoint == true)
+        {
+            controller.positiveZRotation(wristVertJoint);
+        }
+    }
+
+    //JOINT SELECTION FUNCTIONS
+    public void baseJointSelected()
+    {
+        isBaseJoint = true;
+        isShoulderJoint = false;
+        isElbowJoint = false;
+        isWristVertJoint = false;
+
+        //highlight the seelcted joint on the robot arm. swapping materials to green and keep the unselected with default orange
+        baseRend.material = selectedMaterial;
+        ShoulderRend.material = defaultMaterial;
+        elbowRend.material = defaultMaterial;
+        wristVertRend.material = defaultMaterial;
+
+        //Disable Unusable movement keys and enable usable keys
+        //enable - + Y keys
+        minYButton.SetActive(true);
+        posYButton.SetActive(true);
+        //disable - + Z keys
+        minZButton.SetActive(false);
+        posZButton.SetActive(false);
+    }
+
+    public void shoulderJointSelected()
+    {
+        isBaseJoint = false;
+        isShoulderJoint = true;
+        isElbowJoint = false;
+        isWristVertJoint = false;
+
+        //highlight the seelcted joint on the robot arm. swapping materials to green and keep the unselected with default orange
+        baseRend.material = defaultMaterial;
+        ShoulderRend.material = selectedMaterial;
+        elbowRend.material = defaultMaterial;
+        wristVertRend.material = defaultMaterial;
+
+        //Disable Unusable movement keys and enable usable keys
+        //disable - + Y keys
+        minYButton.SetActive(false);
+        posYButton.SetActive(false);
+        //enable - + Z keys
+        minZButton.SetActive(true);
+        posZButton.SetActive(true);
+
+    }
+
+    public void elbowJointSelected()
+    {
+        isBaseJoint = false;
+        isShoulderJoint = false;
+        isElbowJoint = true;
+        isWristVertJoint = false;
+
+        //highlight the seelcted joint on the robot arm. swapping materials to green and keep the unselected with default orange
+        baseRend.material = defaultMaterial;
+        ShoulderRend.material = defaultMaterial;
+        elbowRend.material = selectedMaterial;
+        wristVertRend.material = defaultMaterial;
+
+        //Disable Unusable movement keys and enable usable keys
+        //disable - + Y keys
+        minYButton.SetActive(false);
+        posYButton.SetActive(false);
+        //enable - + Z keys
+        minZButton.SetActive(true);
+        posZButton.SetActive(true);
+    }
+
+    public void wristVertJointSelected()
+    {
+        isBaseJoint = false;
+        isShoulderJoint = false;
+        isElbowJoint = false;
+        isWristVertJoint = true;
+
+
+        //highlight the seelcted joint on the robot arm. swapping materials to green and keep the unselected with default orange
+        baseRend.material = defaultMaterial;
+        ShoulderRend.material = defaultMaterial;
+        elbowRend.material = defaultMaterial;
+        wristVertRend.material = selectedMaterial;
+
+        //Disable Unusable movement keys and enable usable keys
+        //disable - + Y keys
+        minYButton.SetActive(false);
+        posYButton.SetActive(false);
+        //enable - + Z keys
+        minZButton.SetActive(true);
+        posZButton.SetActive(true);
     }
 }
